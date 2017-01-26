aa641a1e820336a46c6de5415e74cd0894d95235
Emily Turner-Sherwood <emilyturnerr@gmail.com>
2017-01-26 15:16:03 -0600

    calling the orm functions

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,28 @@
 // Importing the connection.js file
 var connectionFile = require('../config/connection.js');
+// Importing the orm.js file
 var orm = require("../config/orm.js");
 
-//CALL ORM.JS FUNCTIONS HERE
+//Calling the ORM functions
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
 
 // Exporting the file
 module.exports = burger;
