1f2836e0cb4416fa1e2ce414c52b9ad5fbccf933
Emily Turner-Sherwood <emilyturnerr@gmail.com>
2017-01-26 14:43:52 -0600

    orm

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,4 +9,37 @@ router.get('/', function (req, res) {
 	res.send('/burger');
 });
 
+// Create code that will call the query functions 
+// Using burger specific input
 
+//ALL ROUTER CODE HERE
+
+// Serve index.handlebars to the root route
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM burgers;", function(err, data) {
+    if (err) {
+      throw err;
+    }
+    res.render("index", { burgers: data });
+  });
+});
+// Query functions
+app.post("/burger/", function(req, res) {
+  connection.query("INSERT INTO burgers (burger_name, devoured) VALUES (?)", [
+    req.body.burger_name, req.body.devoured
+  ], function(err, result) {
+    if (err) {
+      throw err;
+    }
+    res.redirect("/");
+  });
+});
+
+app.delete("/burger/:burger_name", function(req, res) {
+  connection.query("DELETE FROM burgers WHERE burger_name = ?", [req.params.burger_name], function(err, result) {
+    if (err) {
+      throw err;
+    }
+    res.redirect("/");
+  });
+});
--- models/burger.js
+++ models/burger.js
@@ -1,38 +1,8 @@
 // Importing the connection.js file
 var connectionFile = require('../config/connection.js');
+var orm = require("../config/orm.js");
 
-// Create code that will call the query functions 
-// Using burger specific input
-
-// Serve index.handlebars to the root route
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM burgers;", function(err, data) {
-    if (err) {
-      throw err;
-    }
-    res.render("index", { burgers: data });
-  });
-});
-
-app.post("/", function(req, res) {
-  connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [
-    req.body.burger_name, req.body.devoured
-  ], function(err, result) {
-    if (err) {
-      throw err;
-    }
-    res.redirect("/");
-  });
-});
-
-app.delete("/:burger_name", function(req, res) {
-  connection.query("DELETE FROM burgers WHERE burger_name = ?", [req.params.burger_name], function(err, result) {
-    if (err) {
-      throw err;
-    }
-    res.redirect("/");
-  });
-});
+//CALL ORM.JS FUNCTIONS HERE
 
 // Exporting the file
 module.exports = burger;
